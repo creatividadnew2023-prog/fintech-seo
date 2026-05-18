c0a0c0e0af81d20920f8ab523483f33ec846f73e
creatividadnew2023-prog <creatividadnew2023-prog@users.noreply.github.com>
2026-05-18 15:17:43 -0400

    Add astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://caribeantrade.online',
+  integrations: [sitemap()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
